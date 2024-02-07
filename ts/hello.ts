58efa3b403b293e1052242678a4f042f1933b553
seoyeon08 <silversun0808@gmail.com>
2024-02-07 16:05:54 +0900

    [Add] 0207

---
+++ ts/hello.ts
@@ -0,0 +1,33 @@
+console.log('Hello Typesctipt!');
+
+type User = {
+    id: number;
+    name: string;
+};
+
+const hong: User = {id: 1, name: 'Hong'};
+const kim: User = {id: 2, name: 'Kim'};
+
+const s: string = 'abc';
+let i = 1;
+const n: null = null;
+const u: undefined = undefined;
+
+type Member = {
+    name: string;
+    addr: string;
+    discountRage: number;
+};
+type Guest = {
+    name: string;
+    age: number;
+};
+
+type Customer = Member | Guest;
+
+let customer: Customer;
+customer = {
+    name: '홍길동',
+    addr: '용산구',
+    age: 26
+}
